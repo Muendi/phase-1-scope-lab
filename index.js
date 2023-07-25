08530bdafda124d1b8f421bd85badc6c7147fdd5
Michelle Kyalo <muendikyalo@gmail.com>
2023-07-25 12:16:38 +0300

    Done with scope lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = "bob";
+customerName();
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+setBestCustomer();
+
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer;
+} 
+setBestCustomer('maybe bob');
+
+const leastFavoriteCustomer = "muendi";
+function changeLeastFavoriteCustomer(){
+    throw new Error('Assignment to constant variable.')
+    leastFavoriteCustomer = "Kyalo";
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
+
+
